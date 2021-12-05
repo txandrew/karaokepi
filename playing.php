662ff5fc967e3d0a6c769ea6452950ad6745e53c
txandrew <34613977+txandrew@users.noreply.github.com>
2021-12-05 17:26:21 -0600

    Make Player Icons Bigger

--- playing.php
+++ playing.php
@@ -88,9 +88,9 @@ function draw_Ratings()
 </script>
 <h1><?php echo $row["status"]; ?></h1>
 <div style="text-align:center">
-<button onclick="sendCmd('PAUSED');" style="width:22%"><i class="material-icons">pause_circle_filled</i></button>
-<button onclick="sendCmd('PLAYED');"  style="width:22%"><i class="material-icons">play_circle_filled</i></button>
-<button onclick="sendCmd('SKIPPING');"  style="width:22%"><i class="material-icons">skip_next</i></button>
+<i class="material-icons" onclick="sendCmd('PAUSED');" style="font-size:40px">pause_circle_filled</i></button>
+<i class="material-icons" onclick="sendCmd('PLAYED');" style="font-size:40px">pause_circle_filled</i></button>
+<i class="material-icons" onclick="sendCmd('SKIPPING');" style="font-size:40px">skip_next</i></button>
 </div>
 <h2>
 <?php 
