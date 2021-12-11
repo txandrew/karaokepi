34d2230abf2dcadb61439f8f2eb533270a8e590e
txandrew <Anderson.Andrew.R@gmail.com>
2021-12-11 11:36:44 -0600

    Add User Interface for Upload

---
+++ upload.php
@@ -0,0 +1,12 @@
+<?php
+include "header.php";
+?>
+
+<form action="upload.php" method="post" enctype="multipart/form-data">
+Select MP4 to upload:
+<input type="file" name="fileToUpload" id="fileToUpload">
+<input type="submit" value="Upload Image" name="submit">
+</form>
+
+</body>
+</html>
